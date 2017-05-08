d9cccd31b97dca0ac83a90de54273885fe7fe9e8
Deepak Rameshkumar <dr439@afsconnect2.njit.edu>
2017-05-08 18:08:20 -0400

    register add

---
+++ register.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<div class="body"></div>
+<div class="grad"></div>
+<br>
+<div class="login">
+<form class="form-login" method="post" action="./controller/index.php">
+<input type="text" placeholder="First Name" name="firstname"><br>
+<input type="text" placeholder="Last Name" name="lastname"><br>
+<input type="text" placeholder="E-mail" name="mailid"><br>
+<input type="text" placeholder="Contact No" name="contact"><br>
+<input type="text" placeholder="username" name="user"><br>
+<input type="password" placeholder="password" name="password"><br>	
+<input type="hidden" name="action" value="register">
+<input type="submit" value="Sign-Up">
+</form>
+</div>
+</body>
+</html>
+												
